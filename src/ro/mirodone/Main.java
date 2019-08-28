fd55e4a8d8915b80e4be1bc6b1395da66e138c1c
apartment81 <bogdan.mirodone@gmail.com>
2019-08-29 00:06:01 +0300

    clever solution

--- src/ro/mirodone/Main.java
+++ src/ro/mirodone/Main.java
@@ -8,6 +8,10 @@ public class Main {
         Scramblies scramblies = new Scramblies();
         System.out.println(scramblies.scramble("scriptingjava", "javascript"));
 
+        CleverScramblies scramblies1 = new CleverScramblies();
+
+        System.out.println(scramblies1.scramble("scriptingjava", "javascript"));
+
 
     }
 }
